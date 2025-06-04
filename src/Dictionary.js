34e98abe528ef34717cdfe6642ddb162992beb75
Kanakosaitoh <kanahanasasha@gmail.com>
2025-06-04 12:16:47 -0700

    changed from button to input submit

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,7 +15,7 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={changeKeyword} />
-        <button value="Search" className="search-btn m-2 " />
+        <inout type="submit" value="Search" className="search-btn m-2 " />
       </form>
     </div>
   );

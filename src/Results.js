8f411e0685d13e3424362bde0641782ad248bbeb
Kanakosaitoh <kanahanasasha@gmail.com>
2025-06-05 13:37:42 -0700

    deleted [0]

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.antonyms[0]}</h2>
+        <h2>{props.results.antonyms}</h2>
         <h2>
           <em>{props.results.partOfSpeech}</em>
         </h2>
